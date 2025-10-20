6b3f2610103fd1fb8c83bc1df668f71142c35395
douglasswng <douglasswng@gmail.com>
2025-10-20 10:32:45 +0800

    test math notation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { remarkWikiLink } from "./src/lib/remark-wiki-link.mjs";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://douglasswng.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
-    remarkPlugins: [remarkWikiLink],
+    remarkPlugins: [remarkWikiLink, remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
