23f3a2a1336e292858224566a075258d4df8f909
douglasswng <douglasswng@gmail.com>
2025-10-20 11:01:55 +0800

    working latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { remarkWikiLink } from "./src/lib/remark-wiki-link.mjs";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://douglasswng.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
-    remarkPlugins: [remarkWikiLink, remarkMath],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkWikiLink],
+    rehypePlugins: [],
   },
 });
